1c0d532acd6b63cb1789b92c708f29d4bc4b8f14
Babin Bohora <bohorababin1@gmail.com>
2022-06-11 15:23:34 +0900

    current song component created

---
+++ src/components/CurrentSong.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="current-song-box">
+    <h2>{{ song.name }}</h2>
+    <p>{{ song.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "current-song",
+  props: ["song"],
+};
+</script>
+
+<style lang="scss" scoped>
+.current-song-box {
+  text-align: center;
+}
+</style>
