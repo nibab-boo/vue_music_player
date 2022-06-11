6ce0366a5dccacb5694827d72d84c8401dc7f9ca
Babin Bohora <bohorababin1@gmail.com>
2022-06-11 11:50:29 +0900

    first tailwind component added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+// fontawesome import
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash, faPause, faPlay } from "@fortawesome/free-solid-svg-icons";
+library.add(faTrash, faPause, faPlay);
+
 import "./styles/index.scss";
 
 createApp(App).mount("#app");
